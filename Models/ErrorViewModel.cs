2da8f2b7b723761b361f0978a235b5294d33940a
ULISES ANIBAL BAAMONDE <45687573@ort>
2022-10-03 07:54:14 -0300

    First commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace EjemploGithub.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
